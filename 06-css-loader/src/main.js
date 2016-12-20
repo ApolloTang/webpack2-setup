fa900874dce729d067c5beec700081dac4309fa1
Apollo Tang <tang.apollo@gmail.com>
2016-12-20 10:55:02 -0500

    added css loader

--- 06-css-loader/src/main.js
+++ 06-css-loader/src/main.js
@@ -5,6 +5,9 @@ console.log('log from main');
 // elem.src = require('./images/img_01.png');
 // document.getElementById("app-container").appendChild(elem);
 
+var style_main = require('./main.css');
+console.log('style_main: ', style_main);
+
 var elem_twitterImg = require('./modules/twitter');
 document.getElementById("app-container").appendChild(elem_twitterImg);
 
