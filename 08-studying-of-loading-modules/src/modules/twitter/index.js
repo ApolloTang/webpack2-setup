d3d740c99bd391470d647280eb4e91f8264beb7a
Apollo Tang <tang.apollo@gmail.com>
2016-12-20 17:24:20 -0500

    studying module loading

---
+++ 08-studying-of-loading-modules/src/modules/twitter/index.js
@@ -0,0 +1,8 @@
+console.log('log from twitter/index.js');
+var elem = document.createElement("img");
+var style = require('./index.css');
+console.log('style in modules/twitter: ', style);
+elem.className = style['module-style'];
+elem.src = require('images/img_01.png');
+module.exports = elem;
+
