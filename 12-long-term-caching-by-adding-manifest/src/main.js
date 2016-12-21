48920d6ae1b11567506a4ea2953e1e32284b87e6
Apollo Tang <tang.apollo@gmail.com>
2016-12-21 17:46:05 -0500

    long term caching

---
+++ 12-long-term-caching-by-adding-manifest/src/main.js
@@ -0,0 +1,36 @@
+console.log('log from main');
+
+var style_main = require('./main.css');
+// console.log('style_main: ', style_main);
+
+var helper = require('util/helper');
+console.log('helper: ', helper )
+
+
+var _ = require('lodash');
+console.log('_.VERSION: ', _.VERSION);
+
+var myVendor = require('./vendor/my-vendor.js');
+console.log('myVendor.name: ', myVendor.name);
+
+
+// =========================================================
+// 09 code spliting, lazy load with System.import()
+
+
+// var elem_twitterImg = require('./modules/twitter');
+// document.getElementById("app-container").appendChild(elem_twitterImg);
+
+const appContainer = document.getElementById("app-container");
+var btn = document.createElement("button");
+btn.innerHTML = 'click to lazy load';
+appContainer.appendChild(btn);
+btn.addEventListener('click', function(){
+    console.log('click');
+    System.import('./modules/twitter').then( (elem_twitterImg) => {
+        console.log('elem_twitterImg: ', elem_twitterImg);
+        document.getElementById("app-container").appendChild(elem_twitterImg);
+    });
+});
+
+
