1c32f161e55568661d255ff32d7253ed39bae07e
Apollo Tang <tang.apollo@gmail.com>
2016-12-22 16:53:41 -0500

    17-uglify

---
+++ 17-uglify/src/util/helper.js
@@ -0,0 +1,7 @@
+console.log('log from util/helper.js');
+
+export {used, notUsed, constants }
+
+function used () { return 'used' }
+function notUsed () {return 'notUsed'}
+const constants = { a:'a', b:'b' };
