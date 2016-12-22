0ce37c29599c93a0d29fbd4359f71922138f93c5
Apollo Tang <tang.apollo@gmail.com>
2016-12-22 15:02:34 -0500

    added 15 tree shaking

---
+++ 15-tree-shaking/src/main.js
@@ -0,0 +1,43 @@
+console.log('log from main');
+
+var style_main = require('./main.css');
+
+import {used, constants} from 'util/helper';
+console.log('helper.constants: ', constants );
+console.log('helper.used: ', used() );
+
+import _ from 'lodash';
+console.log('_.VERSION: ', _.VERSION);
+
+import myVendor from  './vendor/my-vendor.js';
+console.log('myVendor.name: ', myVendor.name);
+
+
+// =========================================================
+// 09 code spliting, lazy load with System.import()
+
+// var elem_twitterImg = require('./modules/twitter');
+// document.getElementById("app-container").appendChild(elem_twitterImg);
+const appContainer = document.getElementById("app-container");
+var btn = document.createElement("button");
+btn.innerHTML = 'click to lazy load';
+appContainer.appendChild(btn);
+btn.addEventListener('click', function(){
+    System.import('./modules/twitter').then(
+        ({ default: elem_twitterImg }) => {
+            console.log('elem_twitterImg: ', elem_twitterImg);
+            document.getElementById("app-container").appendChild(elem_twitterImg);
+        }
+    );
+});
+
+
+// =========================================================
+// 13-ES6
+//
+const n = {o: 'o', p:'p'};
+const m = {...n};
+const testSpread = () => ({...n});
+console.log ('test es7 spread: ', testSpread());
+
+
---
+++ 15-tree-shaking/src/vendor/my-vendor.js
@@ -0,0 +1,3 @@
+console.log('log from vendor/my-vendor.js');
+const myVendor = { name:'myVendor' };
+export default myVendor;
