af73c2d63bb60d26b1a62b49b7fa012bb376cbd3
Apollo Tang <tang.apollo@gmail.com>
2016-12-28 11:50:04 -0500

    common folder now package into its own bundle

---
+++ 32-common-folder-now-package-into-its-own-bundle/src/modules/module-a/index.js
@@ -0,0 +1,17 @@
+console.log('log from modules/module-a/index.js');
+
+import React, {Component} from  'react';
+import style from './index.less';
+
+
+class ModuleA extends Component {
+    render() {
+        return (
+            <div className={style['module-A']}>
+                <p>This is module A</p>
+            </div>
+       );
+    }
+};
+
+export default ModuleA;
---
+++ 32-common-folder-now-package-into-its-own-bundle/webpack.config.js
@@ -0,0 +1,151 @@
+const pathResolve = require('path').resolve;
+// const webpackValidator = require('webpack-validator');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const {getIfUtils, removeEmpty} = require('webpack-config-utils');
+const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
+
+
+// These path must be absolute path on running machine
+const absolutePathToSourceFolder = pathResolve('src');
+const absolutePathToBuildFolder = pathResolve('dist');
+const absolutePathToNodeModules = pathResolve('./node_modules');
+
+module.exports = env => {
+    console.log('env: ', env);
+    const {ifProd, ifNotProd} = getIfUtils(env);
+
+
+    console.log( '----- path info -----');
+    console.log('__dirname: ', __dirname);
+    console.log('absolutePathToSourceFolder: ',  absolutePathToSourceFolder );
+    console.log('absolutePathToBuildFolder: ', absolutePathToBuildFolder );
+    console.log('absolutePathToNodeModules: ', absolutePathToNodeModules );
+    console.log( '----- path info end -----');
+
+    let config = {
+        devServer: {
+            hot: ifNotProd(),
+            host: '0.0.0.0',
+            historyApiFallback: true,
+        },
+        context: absolutePathToSourceFolder, // context of entrypoint
+        entry: {
+            // Note: Each key in entry will map to the [name] placeholder in the value of output.filename
+            vendor: [
+                'lodash', // load from node_modules
+                './vendor/my-vendor',
+                './vendor/oban'
+            ],
+            common: [
+                './common/styles/index.less'
+            ],
+            main: removeEmpty([
+                ifNotProd('webpack-dev-server/client?http://0.0.0.0:8080'),
+                ifNotProd('webpack/hot/only-dev-server'),
+                // ifNotProd('webpack/hot/dev-server'),
+                ifNotProd('react-hot-loader/patch'),
+                './main',
+            ])
+        },
+        output: {
+            pathinfo: ifNotProd(),
+            path: absolutePathToBuildFolder,
+            filename: ifProd('bundle.[name].[chunkhash].js', 'bundle.[name].js')
+        },
+        resolve: {
+            // http://moduscreate.com/es6-es2015-import-no-relative-path-webpack/
+            // http://stackoverflow.com/questions/27502608/resolving-require-paths-with-webpack
+            // https://gist.github.com/sokra/27b24881210b56bbaff7#resolving-options
+            // https://github.com/webpack/enhanced-resolve
+            modules: [
+                absolutePathToSourceFolder,
+                absolutePathToNodeModules
+            ],
+            extensions: ['.js', '.jsx', '.json', '.css', '.less'] // enables users to leave off the extension when importing
+        },
+        module: {
+            loaders: removeEmpty([
+                {
+                    test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+                    loader: 'file-loader?name=[name].[ext]'  // <-- retain original file name
+                    //https://github.com/coryhouse/react-slingshot/issues/128
+                },
+                ifProd(
+                    {
+                      test: /\.less$/,
+                      loader: ExtractTextPlugin.extract({
+                          fallbackLoader: 'style-loader',
+                          loader: 'css-loader?modules!less-loader',
+                          // loader: 'css',
+                      }),
+                      include: absolutePathToSourceFolder
+                    },
+                    {
+                      test: /\.less$/,
+                      loader: 'style-loader!css-loader?modules!less-loader',
+                      include: absolutePathToSourceFolder
+                    }
+                ),
+                {
+                    test: /\.jsx?$/,
+                    loaders: 'babel-loader',
+                    exclude: /node_modules/
+                }
+            ]),
+        },
+        plugins: removeEmpty([
+            new ProgressBarPlugin(),
+            new HtmlWebpackPlugin({
+                template: './index.template.html',
+                favicon: './images/favicon.ico'
+                // inject: 'head',
+            }),
+            ifProd( new ExtractTextPlugin('styles.[name].[chunkhash].css')),
+            ifProd(
+                new webpack.optimize.CommonsChunkPlugin({
+                    name: [   // Specify the common bundle's name.
+                        'vendor',
+                        'common',
+                        'manifest'
+                    ]
+                }),
+                new webpack.optimize.CommonsChunkPlugin({
+                    name: [
+                        'vendor', // Specify the common bundle's name.
+                        'common'
+                    ]
+                })
+            ),
+            ifNotProd( new webpack.HotModuleReplacementPlugin() ),
+            new webpack.NamedModulesPlugin(),
+            ifProd( new InlineManifestWebpackPlugin() )
+        ]),
+        ////// The next configuration show es5 source code during development mode
+        // devtool: ifProd(
+        //     'source-map',     // production enviroment: source map in separate file
+        //     'eval'            // non production env:    inline source map
+        // )
+        // ---------
+        ////// The next configuration show no code original code during development mode
+        devtool: ifProd(
+            'source-map',     // production enviroment: source map in separate file
+            'eval-source-map' // non production env:    inline source map
+        )
+        // ---------
+        ////// The next configuration won't pause in debugger, and show no code
+        // devtool: ifProd(
+        //     'cheap-module-eval-source-map',     // production enviroment: source map in separate file
+        //     'cheap-module-eval-source-map'      // non production env:    inline source map
+        // )
+    };
+    if (env && env.debug) {
+        console.log('webpack.config: ', config)
+    }
+    // return webpackValidator(config);
+    return config;
+}
+
+
